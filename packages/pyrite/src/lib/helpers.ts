1cfea99a1d185625202e80ad565286a7d0c9d3ec
Jeroen van Veen <jvanveen@protonmail.com>
2026-02-13 08:29:07 +0100

    feat: add oxfmt

--- packages/pyrite/src/lib/helpers.ts
+++ packages/pyrite/src/lib/helpers.ts
@@ -1,6 +1,5 @@
-/* eslint-disable no-extend-native */
 // @ts-expect-error - Extending Number prototype for decimals helper
 Number.prototype.decimals = function(decimals: number) {
-    return Number(Math.round(Number(this+'e'+decimals))+'e-'+decimals)
+    return Number(Math.round(Number(this + 'e' + decimals)) + 'e-' + decimals)
 }
-/* eslint-enable no-extend-native */
+
